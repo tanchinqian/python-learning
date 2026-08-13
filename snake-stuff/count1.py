cdbc0ed01af136891e4aada144c1c3636a60fa8b
Tan Chin Qian <tanchinqian@gmail.com>
2026-08-13 21:43:00 +0800

    learned how to enumerate and zip two lists at once

--- snake-stuff/count1.py
+++ snake-stuff/count1.py
@@ -36,4 +36,20 @@ def find_max(dict_temp):
   return  max_item , max_value
 
 max_item , max_value = find_max(exp_dict)
-print(f'The maximum item is {max_item} with the count of {max_value}')
+###print(f'The maximum item is {max_item} with the count of {max_value}')
+
+student_names = ["Alice", "Bob", "Charlie"]
+student_scores = [85, 42, 90]
+
+def generate_report(names,scores):
+  status = ""
+  for item , (name , status_amount) in enumerate(zip(student_names,student_scores)):
+    if status_amount >= 50:
+      status = "PASS"
+    else:
+      status = "FAIL"
+      
+    print(f'Rank {item + 1} : {name} - {status}' )
+
+
+generate_report(student_names,student_scores)
